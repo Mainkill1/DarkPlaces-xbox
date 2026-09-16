32b4a851a52a290d8325c8d7363ee30200804cd3
Mainkill1 <Mainkill1@gmail.com>
2026-09-16 19:00:19 -0400

    feat: pin legacy renderer dependencies for playable Xbox target [skip ci]

---
+++ xbox/classic/versions.mk
@@ -0,0 +1,8 @@
+# Pinned dependencies for the playable Nexuiz Classic Xbox target.
+# The DarkPlaces revision is from the Nexuiz 2.5.2 release date and still
+# contains the maintained GL1.1/GL1.3 fixed-function paths that map onto NV2A.
+DP_CLASSIC_REV := 7349e20fba3c2b469616505907109863e8cb4a9e
+PBGL_REV := 017ab17c4530bf3e2ad446a50de9b18ea6011548
+OGG_REV := e1774cd77f471443541596e09078e78fdc342e4f
+VORBIS_REV := 0657aee69dec8508a0011f47f3b69d7538e9d262
+NEXUIZ_252_MD5 := d750bc328e58df8492f8d88bdcf818cb
