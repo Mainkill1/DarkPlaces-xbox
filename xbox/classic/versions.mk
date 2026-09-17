105647401896ce285397318012c168d806289272
Mainkill1 <Mainkill1@gmail.com>
2026-09-17 08:02:02 -0400

    build: align classic pins with release lock

--- xbox/classic/versions.mk
+++ xbox/classic/versions.mk
@@ -3,8 +3,10 @@
 # contains the maintained GL1.1/GL1.3 fixed-function paths that map onto NV2A.
 DP_CLASSIC_REV := 7349e20fba3c2b469616505907109863e8cb4a9e
 PBGL_REV := 017ab17c4530bf3e2ad446a50de9b18ea6011548
-OGG_REV := e1774cd77f471443541596e09078e78fdc342e4f
+# Approved production audio decoder baseline: libogg 1.3.6 + libvorbis 1.3.7.
+OGG_REV := be05b13e98b048f0b5a0f5fa8ce514d56db5f822
 VORBIS_REV := 0657aee69dec8508a0011f47f3b69d7538e9d262
 NEXUIZ_252_URL := https://sourceforge.net/projects/nexuiz/files/NexuizRelease/Nexuiz%202.5.2/nexuiz-252.zip/download
+NEXUIZ_252_BYTES := 931253731
 NEXUIZ_252_SHA256 := a5e27ebcc9775c4a490d0d3536c32e4a8f8f96b038c0b6a78d1823c37a962000
 NEXUIZ_252_MD5 := d750bc328e58df8492f8d88bdcf818cb
